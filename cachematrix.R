6032ff702849f5421c6279cf2e8ed223366e7a66
kristin-kirby <kkirby23@gmail.com>
2017-01-18 07:59:48 -0500

    modify cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,56 @@
 ## functions do
 
 ## Write a short comment describing this function
+## makeCacheMatrix returns a list of functions that will get and set
+## the value of the matrix and the value of the matrix inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+      ## return character string that matrix is not invertible if it is not invertible
+      if (class(try(solve(x),silent=T))=="try-error") {
+            errormessage <- "Matrix is not invertible"
+            return(errormessage)
+      }
+      ##  if matrix is invertible, continue
+      else {
+            inv <- NULL 
+            set <- function(y) {
+                  x <<- y 
+                  inv <<- NULL
+            }
+            get <- function() x
+            setinv <- function(inverse) inv <<- inverse
+            getinv <- function() inv
+            
+            list(set = set, get = get, setinv = setinv, getinv = getinv)
+            
+      }
+      
+      
 
 }
 
 
 ## Write a short comment describing this function
+## The cacheSolve function calculates the inverse of the special matrix it creates in
+## the makeCacheMatrix function. However, before calculating the inverse, it first checks
+## to see if the inverse of the special matrix defined above has already been calculated.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+      
+      if (class(x)=="character") {
+            message(x)
+      }
+      else {
+            inv <- x$getinv()
+            if (!is.null(inv)) {
+                  message("getting cached data")
+                  return(inv)
+            }
+            
+            data <- x$get()
+            inv <- solve(data,...)
+            x$setinv(inv)
+            inv
+      }
 }
